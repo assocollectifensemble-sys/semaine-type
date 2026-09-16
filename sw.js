fa05c37ccb49614e3d33215582079c0601c82bce
Jonathan Bou <assocollectifensemble@users.noreply.github.com>
2026-09-16 20:30:32 +0200

    v3: refonte UI mobile, couleurs par personne, gestes type agenda, garde Tous les deux, semaines reelles

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
-const CACHE='semaine-v2';
-const FILES=['./','./app.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES).catch(()=>{})).then(()=>self.skipWaiting()));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
+const C='semaine-v3-1';
+const A=['./','./index.html','./app.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll(A)).catch(()=>{}))});
+self.addEventListener('activate',e=>{e.waitUntil(
+  caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>clients.claim()))});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(
-    fetch(e.request).then(r=>{
-      const cp=r.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});
-      return r;
-    }).catch(()=>caches.match(e.request,{ignoreSearch:true}).then(m=>m||caches.match('./app.html')))
-  );
-});
+  e.respondWith(fetch(e.request).then(r=>{
+    const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp).catch(()=>{}));return r;
+  }).catch(()=>caches.match(e.request).then(m=>m||caches.match('./app.html'))));});
